0819935b45932681a6a134a59767e7d17b53e048
slpcode <1370808424@qq.com>
2019-07-01 15:01:29 +0800

    注册剩余API服务，并在主类写入@property

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,7 +4,11 @@ namespace Slpcode\WangDianTongSdk;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Slpcode\WangDianTongSdk\Basic\Basic;
 use Slpcode\WangDianTongSdk\Goods\Goods;
+use Slpcode\WangDianTongSdk\Purchase\Purchase;
+use Slpcode\WangDianTongSdk\Refund\Refund;
+use Slpcode\WangDianTongSdk\Stock\Stock;
 use Slpcode\WangDianTongSdk\Trade\Trade;
 
 class ServiceProvider implements ServiceProviderInterface
@@ -20,10 +24,10 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $pimple)
     {
-        // 订单接口
-        $pimple['trade'] = function ($pimple) {
+        // 基础接口
+        $pimple['basic'] = function ($pimple) {
             $config = $pimple->getConfig();
-            return new Trade($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
+            return new Basic($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
         };
 
         // 货品接口
@@ -31,5 +35,31 @@ class ServiceProvider implements ServiceProviderInterface
             $config = $pimple->getConfig();
             return new Goods($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
         };
+
+        // 采购接口
+        $pimple['purchase'] = function ($pimple) {
+            $config = $pimple->getConfig();
+            return new Purchase($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
+        };
+
+        // 售后接口
+        $pimple['refund'] = function ($pimple) {
+            $config = $pimple->getConfig();
+            return new Refund($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
+        };
+
+        // 库存接口
+        $pimple['stock'] = function ($pimple) {
+            $config = $pimple->getConfig();
+            return new Stock($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
+        };
+
+        // 订单接口
+        $pimple['trade'] = function ($pimple) {
+            $config = $pimple->getConfig();
+            return new Trade($config['appkey'], $config['appsecret'], $config['sid'], $config['baseUrl']);
+        };
+
+
     }
 }
--- src/WangDianTongSdk.php
+++ src/WangDianTongSdk.php
@@ -6,8 +6,12 @@ namespace Slpcode\WangDianTongSdk;
 use Hanson\Foundation\Foundation;
 
 /**
- * @property \Slpcode\WangDianTongSdk\Trade\Trade $trade
+ * @property \Slpcode\WangDianTongSdk\Basic\Basic $basic
  * @property \Slpcode\WangDianTongSdk\Goods\Goods $goods
+ * @property \Slpcode\WangDianTongSdk\Purchase\Purchase $purchase
+ * @property \Slpcode\WangDianTongSdk\Refund\Refund $refund
+ * @property \Slpcode\WangDianTongSdk\Stock\Stock $stock
+ * @property \Slpcode\WangDianTongSdk\Trade\Trade $trade
  *
  * Class WangDianTong
  * @package Slpcode\WangDianTongSdk
